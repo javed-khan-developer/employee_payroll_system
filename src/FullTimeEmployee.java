e9633f5264a9b92099175292ad5c6f448b0abb91
khanjaved443 <57727397+khanjaved443@users.noreply.github.com>
2023-08-09 16:41:40 +0530

    full time employee created

---
+++ src/FullTimeEmployee.java
@@ -0,0 +1,12 @@
+public class FullTimeEmployee extends Employee {
+    private double monthlySalary;
+
+    public FullTimeEmployee(String name, int id, double monthlySalary) {
+        super(name, id);
+        this.monthlySalary = monthlySalary;
+    }
+
+    public double calculateSalary() {
+        return monthlySalary;
+    }
+}
