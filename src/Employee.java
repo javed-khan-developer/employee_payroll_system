f485459a8b1b1d011da96abf4bb9f5b716a1af20
khanjaved443 <57727397+khanjaved443@users.noreply.github.com>
2023-08-09 16:29:28 +0530

    employee class created

---
+++ src/Employee.java
@@ -0,0 +1,24 @@
+abstract class Employee {
+    private String name;
+    private int id;
+
+    public Employee(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getEmployeeName() {
+        return name;
+    }
+
+    public int getEmployeeId() {
+        return id;
+    }
+
+    public abstract double calculateSalary();
+
+    @Override
+    public String toString() {
+        return "Employee[name : " + name + ", id : " + id + ", salary: " + calculateSalary() + "]";
+    }
+}
