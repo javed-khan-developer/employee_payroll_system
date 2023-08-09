ae921fbfb256fda7f14c958fb87ecba3ed024a3e
khanjaved443 <57727397+khanjaved443@users.noreply.github.com>
2023-08-09 17:27:17 +0530

    payroll system created

---
+++ src/PayrollSystem.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class PayrollSystem {
+    private ArrayList<Employee> employeeList;
+
+    public PayrollSystem() {
+        employeeList = new ArrayList<>();
+    }
+
+    public void addEmployee(Employee employee) {
+        employeeList.add(employee);
+    }
+
+    public void removeEmployee(int id) {
+        Employee employeeToRemove = null;
+        for (Employee employee : employeeList) {
+            if (employee.getEmployeeId() == id) {
+                employeeToRemove = employee;
+                break;
+            }
+            if (employeeToRemove != null) {
+                employeeList.remove(employeeToRemove);
+            }
+        }
+    }
+}
