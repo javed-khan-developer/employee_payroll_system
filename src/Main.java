5d9fdb4bb70f062cc913acf431fc24e8b6fc2156
khanjaved443 <57727397+khanjaved443@users.noreply.github.com>
2023-08-09 18:02:05 +0530

    employee list created

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        PayrollSystem payrollSystem = new PayrollSystem();
+        FullTimeEmployee employee1 = new FullTimeEmployee("Rahul", 1, 100000);
+        PartTimeEmployee employee2 = new PartTimeEmployee("Sachin", 2, 500, 4);
+        PartTimeEmployee employee3 = new PartTimeEmployee("Vikas", 3, 300, 5);
+        payrollSystem.addEmployee(employee1);
+        payrollSystem.addEmployee(employee2);
+        payrollSystem.addEmployee(employee3);
+        System.out.println("Employee List : ");
+        payrollSystem.showEmployee();
+        System.out.println("Removing Employee : ");
+        payrollSystem.removeEmployee(2);
+        System.out.println("Remaining Employee List : ");
+        payrollSystem.showEmployee();
+
     }
 }
