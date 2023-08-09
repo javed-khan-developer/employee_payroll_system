789b2db0ee321697ecc1f75a873ac0e26310d7d0
khanjaved443 <57727397+khanjaved443@users.noreply.github.com>
2023-08-09 16:58:45 +0530

    part time employee created

---
+++ src/PartTimeEmployee.java
@@ -0,0 +1,15 @@
+public class PartTimeEmployee extends Employee {
+    private double hoursWorked;
+    private double hourlyRate;
+
+    public PartTimeEmployee(String name, int id, double hourlyRate, double hoursWorked) {
+        super(name, id);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return hoursWorked * hourlyRate;
+    }
+}
